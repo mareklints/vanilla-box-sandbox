853393dd716b659e7f3797aabdf3287367cf5e46
Marek Lints <marek.lints@gmail.com>
2020-04-03 21:17:15 +0300

    Dynamic popup window generation

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,22 @@
 import 'vanilla-box/popup.css';
-import { box } from 'vanilla-box';
-import { config } from 'vanilla-box';
+import 'vanilla-box';
 
-console.log('box', box);
-console.log('config', config);
-
-box.initPopup();
-setTimeout(() => box.openPopup(config.POPUP_CONF_DEFAULT, 'testTemplate'), 1000);
-
+window.addEventListener('load', () => {
+    document.querySelector('#test1').innerHTML = /*html*/`
+        <popup-window config-name="blank_400_400">
+        <template>
+            <div>
+                <h1>Test www</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, provident vero corrupti dolore
+                    pariatur consequatur neque dolor eveniet ut voluptas voluptates voluptatibus, laudantium sequi non
+                    omnis
+                    tempora cum mollitia minima?
+                </p>
+            </div>
+        </template>
+        </popup-window>
+        <button>Open</button>
+    `;
+    document.querySelector('#test1 button').addEventListener('click', () => document.querySelector('#test1 popup-window').open());
+});
